b119267fe7df84258ab2035e3b3d204e145f3c0d
unknown <vbutnaru@endava.com>
2019-10-07 16:37:36 +0300

    Test for validator and StringCreator

---
+++ DiplomaTests/StringCreatorTests.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using Xunit;
+using Diploma.Models;
+using Moq;
+using Diploma.Utils;
+using Diploma.Wrappers;
+using Diploma;
+
+namespace DiplomaTests
+{
+    public class StringCreatorTests
+    {
+        [Fact]
+        public void GetSecondParagraphText_ShoulReturn_CorrectString()
+        {
+            var constants = new Mock<IConstants>();
+
+            var domainWrapper = new Mock<IDomainWrapper>();
+
+            var stringcreator = new StringCreator(constants.Object, domainWrapper.Object);
+
+            Subject limba = new Subject { SubjectName = "Limba romana", Grade = 10 };
+
+            List<Subject> grades = new List<Subject>();
+
+            grades.Add(limba);
+
+            var act = stringcreator.GetSecondParagraphText(grades);
+
+            Assert.Equal("Limba romana 10\n", act);
+        }
+    }
+}
---
+++ DiplomaTests/ValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Diploma.Validators;
+using Diploma.Models;
+
+namespace DiplomaTests
+{
+    public class ValidatorTests
+    {
+        [Fact]
+        public void ValidateStudentRecord_EmptyStudent_ReturnFalse()
+        {
+            Validator validator = new Validator();
+
+            var studentModel = new StudentModel();
+
+            var actual = validator.ValidateStudentRecord(studentModel);
+
+            Assert.Equal(false, actual);
+        }
+
+        [Fact]
+        public void ValidateStudentRecord_WithoudSubject_ReturnFalse()
+        {
+            Validator validator = new Validator();
+
+            StudentModel studentModel = new StudentModel();
+
+            studentModel.FirstName = "Chrila";
+            studentModel.LastName = "Petru";
+            studentModel.Grades = null;
+
+            var act = validator.ValidateStudentRecord(studentModel);
+
+            Assert.Equal(false, act);
+        }
+
+        [Fact]
+        public void ValidateStudentRecord_ShouldNotReturnFalse()
+        {
+            Validator validator = new Validator();
+
+            Subject limba = new Subject { SubjectName = "Limba romana", Grade = 10 };
+
+            List<Subject> grades = new List<Subject>();
+
+            grades.Add(limba);
+
+            var studentModel = new StudentModel { FirstName = "Chrila", LastName = "Petru", Grades = grades};
+
+            var actual = validator.ValidateStudentRecord(studentModel);
+
+            Assert.Equal(true, actual);
+        }
+       
+    }
+}
